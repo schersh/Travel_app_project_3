b713c350656b97e678055a4f4ca5276a803be6a9
Sarah Hersh <schersh@gmail.com>
2015-12-04 15:46:02 -0500

    added one spec file

--- spec/city-spec.js
+++ spec/city-spec.js
@@ -2,24 +2,22 @@
 //   it should have a name
 //   it should belong to a user's list of favorite cities
 //   it should have notes associated
+// it ("should have one or more notes associated", function(){
+//   // 'toContain' matcher is for finding an item in an Array
+//   var city = new City();
+//   var note = new Note;
+//   var notes = [note]
+//   expect(city).toContain(notes);
+// });
+// it ("should belong to a user", function(){
+//
+// });
+
 var City = require("../models/city");
 
 describe ("a city", function(){
   it ("should have a name", function(){
-    //expect city to have a name
-    //expect city name to be defined
-    var city = new City();
-    city.name = "New York";
-    expect(city.name).toBeDefined();
-  });
-  it ("should have one or more notes associated", function(){
-    // 'toContain' matcher is for finding an item in an Array
-    var city = new City();
-    var note = new Note;
-    var notes = [note]
-    expect(city).toContain(notes);
-  });
-  it ("should belong to a user", function(){
-
+    var newYork = new City("New York");
+    expect(newYork.name).toBeDefined();
   });
 });
--- spec/note-spec.js
+++ spec/note-spec.js
@@ -1,10 +1,10 @@
-describe ("a note", function(){
-  it ("should have body text", function(){
-    var note = new Note();
-    expect(city.body).toBeDefined();
-    expect( typeof note.body ).toBe("string");
-  });
-  it ("should belong to a city", function(){
-
-  });
-});
+// describe ("a note", function(){
+//   it ("should have body text", function(){
+//     var note = new Note();
+//     expect(city.body).toBeDefined();
+//     expect( typeof note.body ).toBe("string");
+//   });
+//   it ("should belong to a city", function(){
+//
+//   });
+// });
