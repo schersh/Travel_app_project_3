979ca9b13945db3fd5cebe118e09b230dffcb2fa
Sarah Hersh <schersh@gmail.com>
2015-12-08 16:28:26 -0500

    added flash

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// express dependency for our application
-var express = require('express')
-// loads mongoose dependency
-var router = express.Router();
+// loads dependencies
+var passport    = require('passport');
+var express     = require('express')
+var flash       = require('connect-flash');
 var mongoose = require('mongoose')
-// loads dependency for middleware for paramters
 var bodyParser = require('body-parser')
-// loads dependency that allows put and delete where not supported in html
 var methodOverride = require('method-override')
-// loads module containing all authors controller actions. not defined yet...
+var session      = require('express-session');
+// loads controllers
 var usersController = require("./controllers/usersController")
 var citiesController = require("./controllers/citiesController")
 var notesController = require("./controllers/notesController")
@@ -15,16 +14,15 @@ var notesController = require("./controllers/notesController")
 // mongoose.connect('mongodb://localhost/')
 // invokes express dependency and sets namespace to app
 var app = express()
-// sets view engine to handlebars
+
 app.set("view engine", "hbs")
-// allows for parameters in JSON and html
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
-// allows for put/delete request in html form
 app.use(methodOverride('_method'))
-// connects assets like stylesheets
 app.use(express.static(__dirname + '/public'))
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 // app server located on port 4000
 app.listen(4000, function(){
   console.log("app listening on port 4000")
@@ -56,5 +54,3 @@ app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     next();
   });
-
-module.exports = router;
