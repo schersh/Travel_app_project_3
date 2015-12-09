ac670112fc79babb7149f07e2d47bbd7a6193fc5
Sarah Hersh <schersh@gmail.com>
2015-12-09 10:47:08 -0500

    modified index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 var express     = require('express')
 var session      = require('express-session');
 var flash       = require('connect-flash');
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-var methodOverride = require('method-override')
-var app = express()
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
 var passport    = require('passport');
 require('./config/passport')(passport);
 
@@ -14,23 +14,23 @@ app.use(function (req, res, next) {
     next();
   });
 
-app.set("view engine", "hbs")
+app.set("view engine", "hbs");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(methodOverride('_method'))
-app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
 app.use(passport.initialize());
 app.use(passport.session({secret: "yo"}));
 app.use(flash());
 
 // loads controllers
-var usersController = require("./controllers/usersController")
-var citiesController = require("./controllers/citiesController")
-var notesController = require("./controllers/notesController")
+var usersController = require("./controllers/usersController");
+var citiesController = require("./controllers/citiesController");
+var notesController = require("./controllers/notesController");
 
 app.get("/", function(req, res){
   res.render("index.hbs")
-})
+});
 
 function authenticatedUser(req, res, next) {
   // If the user is authenticated, then we continue the execution
@@ -38,7 +38,7 @@ function authenticatedUser(req, res, next) {
 
   // Otherwise the request is always redirected to the home page
   res.redirect('/');
-}
+};
 
 app.get("/signup", usersController.getSignup);
 app.post("/signup", usersController.postSignup);
