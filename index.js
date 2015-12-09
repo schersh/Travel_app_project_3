4288fe8f02bf7a6a04ca95b0a9f94ad9560502bf
Sarah Hersh <schersh@gmail.com>
2015-12-09 11:06:57 -0500

    debugging local strategy

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.use(passport.initialize());
-app.use(passport.session({secret: "yo"}));
+app.use(session({secret: "yo"}));
 app.use(flash());
 
 // loads controllers
